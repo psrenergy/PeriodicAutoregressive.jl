f287c78db65f8accb5ee12f0c0d68a7254b839b3
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-11-03 15:43:48 -0300

    Project updates

--- test/test.bat
+++ test/test.bat
@@ -1,5 +1,7 @@
 @echo off
 
 SET BASEPATH=%~dp0
+SET JULIA=1.11.7
 
-CALL "%JULIA_1111%" --project=%BASEPATH%\.. -e "import Pkg; Pkg.test()"
+CALL juliaup add %JULIA%
+CALL julia +%JULIA% --project=%BASEPATH%\.. -e "import Pkg; Pkg.test()"
